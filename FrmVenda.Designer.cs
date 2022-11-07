cae54430ccc0fbc4e5aa60e8773a56cc32b9a16b
Eiji <99904315+gotou-eiji@users.noreply.github.com>
2022-11-07 17:08:26 -0300

    Add files via upload

--- FrmVenda.Designer.cs
+++ FrmVenda.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace boteco
+namespace BotecoTDS08
 {
     partial class FrmVenda
     {
@@ -29,12 +29,356 @@ namespace boteco
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblIdPedido = new System.Windows.Forms.Label();
+            this.txtIdPedido = new System.Windows.Forms.TextBox();
+            this.btnNovoPedido = new System.Windows.Forms.Button();
+            this.dgvPedido = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIdProduto = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cbxCliente = new System.Windows.Forms.ComboBox();
+            this.cbxProduto = new System.Windows.Forms.ComboBox();
+            this.btnAtualizarPedido = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnNovoItem = new System.Windows.Forms.Button();
+            this.btnEditarItem = new System.Windows.Forms.Button();
+            this.btnExcluirItem = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnFinalizarVenda = new System.Windows.Forms.Button();
+            this.btnFinalizarPedido = new System.Windows.Forms.Button();
+            this.lblEstoque = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPedido)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblIdPedido
+            // 
+            this.lblIdPedido.AutoSize = true;
+            this.lblIdPedido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdPedido.Location = new System.Drawing.Point(9, 14);
+            this.lblIdPedido.Name = "lblIdPedido";
+            this.lblIdPedido.Size = new System.Drawing.Size(112, 15);
+            this.lblIdPedido.TabIndex = 0;
+            this.lblIdPedido.Text = "Id Pedido/Venda";
+            // 
+            // txtIdPedido
+            // 
+            this.txtIdPedido.Location = new System.Drawing.Point(12, 30);
+            this.txtIdPedido.Name = "txtIdPedido";
+            this.txtIdPedido.Size = new System.Drawing.Size(109, 20);
+            this.txtIdPedido.TabIndex = 1;
+            // 
+            // btnNovoPedido
+            // 
+            this.btnNovoPedido.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnNovoPedido.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNovoPedido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovoPedido.Location = new System.Drawing.Point(599, 65);
+            this.btnNovoPedido.Name = "btnNovoPedido";
+            this.btnNovoPedido.Size = new System.Drawing.Size(135, 36);
+            this.btnNovoPedido.TabIndex = 2;
+            this.btnNovoPedido.Text = "Novo Pedido";
+            this.btnNovoPedido.UseVisualStyleBackColor = true;
+            // 
+            // dgvPedido
+            // 
+            this.dgvPedido.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPedido.Location = new System.Drawing.Point(12, 330);
+            this.dgvPedido.Name = "dgvPedido";
+            this.dgvPedido.Size = new System.Drawing.Size(580, 150);
+            this.dgvPedido.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cliente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Produto";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(12, 287);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(140, 20);
+            this.txtValor.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Valor (R$)";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(12, 234);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(140, 20);
+            this.txtQuantidade.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Quantidade";
+            // 
+            // txtIdProduto
+            // 
+            this.txtIdProduto.Location = new System.Drawing.Point(12, 183);
+            this.txtIdProduto.Name = "txtIdProduto";
+            this.txtIdProduto.Size = new System.Drawing.Size(76, 20);
+            this.txtIdProduto.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "IdProduto";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(174, 234);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Estoque";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(323, 510);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(140, 20);
+            this.txtTotal.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(240, 510);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Total (R$)";
+            // 
+            // cbxCliente
+            // 
+            this.cbxCliente.FormattingEnabled = true;
+            this.cbxCliente.Location = new System.Drawing.Point(11, 80);
+            this.cbxCliente.Margin = new System.Windows.Forms.Padding(2);
+            this.cbxCliente.Name = "cbxCliente";
+            this.cbxCliente.Size = new System.Drawing.Size(411, 21);
+            this.cbxCliente.TabIndex = 18;
+            // 
+            // cbxProduto
+            // 
+            this.cbxProduto.FormattingEnabled = true;
+            this.cbxProduto.Location = new System.Drawing.Point(12, 133);
+            this.cbxProduto.Margin = new System.Windows.Forms.Padding(2);
+            this.cbxProduto.Name = "cbxProduto";
+            this.cbxProduto.Size = new System.Drawing.Size(411, 21);
+            this.cbxProduto.TabIndex = 19;
+            // 
+            // btnAtualizarPedido
+            // 
+            this.btnAtualizarPedido.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAtualizarPedido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtualizarPedido.Location = new System.Drawing.Point(599, 118);
+            this.btnAtualizarPedido.Name = "btnAtualizarPedido";
+            this.btnAtualizarPedido.Size = new System.Drawing.Size(135, 38);
+            this.btnAtualizarPedido.TabIndex = 20;
+            this.btnAtualizarPedido.Text = "Atualizar Pedido";
+            this.btnAtualizarPedido.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(127, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(203, 36);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Localizar Pedido/Venda";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnNovoItem
+            // 
+            this.btnNovoItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNovoItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovoItem.Location = new System.Drawing.Point(296, 181);
+            this.btnNovoItem.Name = "btnNovoItem";
+            this.btnNovoItem.Size = new System.Drawing.Size(140, 37);
+            this.btnNovoItem.TabIndex = 22;
+            this.btnNovoItem.Text = "Novo Item";
+            this.btnNovoItem.UseVisualStyleBackColor = true;
+            // 
+            // btnEditarItem
+            // 
+            this.btnEditarItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditarItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditarItem.Location = new System.Drawing.Point(296, 225);
+            this.btnEditarItem.Name = "btnEditarItem";
+            this.btnEditarItem.Size = new System.Drawing.Size(140, 36);
+            this.btnEditarItem.TabIndex = 23;
+            this.btnEditarItem.Text = "Editar Item";
+            this.btnEditarItem.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluirItem
+            // 
+            this.btnExcluirItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluirItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluirItem.Location = new System.Drawing.Point(296, 267);
+            this.btnExcluirItem.Name = "btnExcluirItem";
+            this.btnExcluirItem.Size = new System.Drawing.Size(140, 37);
+            this.btnExcluirItem.TabIndex = 24;
+            this.btnExcluirItem.Text = "Excluir Item";
+            this.btnExcluirItem.UseVisualStyleBackColor = true;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(626, 444);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(125, 36);
+            this.btnFechar.TabIndex = 25;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // btnFinalizarVenda
+            // 
+            this.btnFinalizarVenda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFinalizarVenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinalizarVenda.Location = new System.Drawing.Point(626, 398);
+            this.btnFinalizarVenda.Name = "btnFinalizarVenda";
+            this.btnFinalizarVenda.Size = new System.Drawing.Size(125, 36);
+            this.btnFinalizarVenda.TabIndex = 26;
+            this.btnFinalizarVenda.Text = "Finalizar Venda";
+            this.btnFinalizarVenda.UseVisualStyleBackColor = true;
+            // 
+            // btnFinalizarPedido
+            // 
+            this.btnFinalizarPedido.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFinalizarPedido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinalizarPedido.Location = new System.Drawing.Point(626, 352);
+            this.btnFinalizarPedido.Name = "btnFinalizarPedido";
+            this.btnFinalizarPedido.Size = new System.Drawing.Size(125, 36);
+            this.btnFinalizarPedido.TabIndex = 27;
+            this.btnFinalizarPedido.Text = "Finalizar Pedido";
+            this.btnFinalizarPedido.UseVisualStyleBackColor = true;
+            // 
+            // lblEstoque
+            // 
+            this.lblEstoque.AutoSize = true;
+            this.lblEstoque.Location = new System.Drawing.Point(245, 234);
+            this.lblEstoque.Name = "lblEstoque";
+            this.lblEstoque.Size = new System.Drawing.Size(0, 13);
+            this.lblEstoque.TabIndex = 28;
+            // 
+            // FrmVenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmVenda";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 551);
+            this.Controls.Add(this.lblEstoque);
+            this.Controls.Add(this.btnFinalizarPedido);
+            this.Controls.Add(this.btnFinalizarVenda);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnExcluirItem);
+            this.Controls.Add(this.btnEditarItem);
+            this.Controls.Add(this.btnNovoItem);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnAtualizarPedido);
+            this.Controls.Add(this.cbxProduto);
+            this.Controls.Add(this.cbxCliente);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtIdProduto);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvPedido);
+            this.Controls.Add(this.btnNovoPedido);
+            this.Controls.Add(this.txtIdPedido);
+            this.Controls.Add(this.lblIdPedido);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmVenda";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "z";
+            this.Load += new System.EventHandler(this.FrmVenda_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPedido)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblIdPedido;
+        private System.Windows.Forms.TextBox txtIdPedido;
+        private System.Windows.Forms.Button btnNovoPedido;
+        private System.Windows.Forms.DataGridView dgvPedido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtIdProduto;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cbxCliente;
+        private System.Windows.Forms.ComboBox cbxProduto;
+        private System.Windows.Forms.Button btnAtualizarPedido;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnNovoItem;
+        private System.Windows.Forms.Button btnEditarItem;
+        private System.Windows.Forms.Button btnExcluirItem;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnFinalizarVenda;
+        private System.Windows.Forms.Button btnFinalizarPedido;
+        private System.Windows.Forms.Label lblEstoque;
     }
 }
--- FrmVenda.cs
+++ FrmVenda.cs
@@ -7,14 +7,160 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
-namespace boteco
+namespace BotecoTDS08
 {
     public partial class FrmVenda : Form
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Boteco\DbBoteco.mdf;Integrated Security=True");
+
         public FrmVenda()
         {
             InitializeComponent();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void CarregaCbxCliente()
+        {
+            string cli = "SELECT Id,nome FROM Cliente";
+            SqlCommand cmd = new SqlCommand(cli, con);
+            con.Open();
+            cmd.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(cli, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "cliente");
+            cbxCliente.ValueMember = "Id";
+            cbxCliente.DisplayMember = "nome";
+            cbxCliente.DataSource = ds.Tables["cliente"];
+            con.Close();
+        }
+
+        public void CarregaCbxProduto()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            string pro = "SELECT Id,nome FROM Produto";
+            SqlCommand cmd = new SqlCommand(pro, con);
+            con.Open();
+            cmd.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(pro, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "produto");
+            cbxProduto.ValueMember = "Id";
+            cbxProduto.DisplayMember = "nome";
+            cbxProduto.DataSource = ds.Tables["produto"];
+            con.Close();
+        }
+
+        private void FrmVenda_Load(object sender, EventArgs e)
+        {
+            CarregaCbxCliente();
+            cbxProduto.Enabled = false;
+            txtIdProduto.Enabled = false;
+            txtQuantidade.Enabled = false;
+            txtValor.Enabled = false;
+            txtTotal.Enabled = false;
+            btnAtualizarPedido.Enabled = false;
+            btnFinalizarPedido.Enabled = false;
+            btnFinalizarVenda.Enabled = false;
+            btnNovoItem.Enabled = false;
+            btnEditarItem.Enabled = false;
+            btnExcluirItem.Enabled = false;
+        }
+
+        private void btnNovoPedido_Click(object sender, EventArgs e)
+        {
+            cbxProduto.Enabled = true;
+            CarregaCbxProduto();
+            txtIdProduto.Enabled = true;
+            txtQuantidade.Enabled = true;
+            txtValor.Enabled = true;
+            txtTotal.Enabled = true;
+            btnAtualizarPedido.Enabled = true;
+            btnFinalizarPedido.Enabled = true;
+            btnFinalizarVenda.Enabled = true;
+            btnEditarItem.Enabled = true;
+            btnNovoItem.Enabled = true;
+            btnExcluirItem.Enabled = true;
+            dgvPedido.Columns.Add("ID", "ID");
+            dgvPedido.Columns.Add("Produto", "Produto");
+            dgvPedido.Columns.Add("Quantidade", "Quantidade");
+            dgvPedido.Columns.Add("Valor", "Valor");
+            dgvPedido.Columns.Add("Total", "Total");
+        }
+
+        private void cbxProduto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Produto WHERE Id=@Id", con);
+            cmd.Parameters.AddWithValue("@Id", cbxProduto.SelectedValue);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                txtValor.Text = dr["preco"].ToString();
+                txtIdProduto.Text = dr["Id"].ToString();
+                txtQuantidade.Focus();
+                dr.Close();
+                con.Close();
+            }
+            con.Open();
+            SqlCommand cmd2 = new SqlCommand("Quantidade_Produto", con);
+            cmd2.Parameters.AddWithValue("@Id", txtIdProduto.Text);
+            cmd2.CommandType = CommandType.StoredProcedure;
+            SqlDataReader dr2 = cmd2.ExecuteReader();
+            if (dr2.Read())
+            {
+                lblEstoque.Text = dr2["quantidade"].ToString();
+            }
+            dr2.Close();
+            con.Close();
+        }
+
+        private void btnNovoItem_Click(object sender, EventArgs e)
+        {
+            var repetido = false;
+            foreach (DataGridViewRow dr in dgvPedido.Rows)
+            {
+                if (txtIdProduto.Text == Convert.ToString(dr.Cells[0].Value))
+                {
+                    repetido = true;
+                }
+            }
+            if (repetido == false)
+            {
+                DataGridViewRow item = new DataGridViewRow();
+                item.CreateCells(dgvPedido);
+                item.Cells[0].Value = txtIdProduto.Text;
+                item.Cells[1].Value = cbxProduto.Text;
+                item.Cells[2].Value = txtQuantidade.Text;
+                item.Cells[3].Value = txtValor.Text;
+                item.Cells[4].Value = Convert.ToDecimal(txtValor.Text) * Convert.ToDecimal(txtQuantidade.Text);
+                dgvPedido.Rows.Add(item);
+                txtIdProduto.Text = "";
+                txtValor.Text = "";
+                txtQuantidade.Text = "";
+                cbxProduto.Text = "";
+                decimal soma = 0;
+                foreach (DataGridViewRow dr in dgvPedido.Rows)
+                    soma += Convert.ToDecimal(dr.Cells[4].Value);
+                txtTotal.Text = Convert.ToString(soma);
+            }
+            else
+            {
+                MessageBox.Show("Produto já cadastrado!", "Produto repetido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
